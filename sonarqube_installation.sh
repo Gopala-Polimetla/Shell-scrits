b7125299a084a7be20f6e64fed5d771b7d986873
Gopala-Polimetla <91300247+Gopala-Polimetla@users.noreply.github.com>
2022-09-12 16:59:17 +0530

    Update sonarqube_installation.sh

--- sonarqube_installation.sh
+++ sonarqube_installation.sh
@@ -9,25 +9,25 @@ echo "updated"
 #echo "groupadded"
 #u need the user as sunarqube in sunarqube group only
 # directly u creat first group then u add user to group while creating
-groupadd sonarqube
-useradd sonarqube -g sonarqube
+groupadd gopala
+useradd gopala -g gopala
 echo "group and user created"
 #to check the user that created by u is present in that specifed group the command given below
 #cat /etc/passwd | awk -F':' '{ print $1}' | xargs -n1 groups
-cat /etc/passwd | awk -F':' '{ print $1}' | xargs -n1 groups
+#cat /etc/passwd | awk -F':' '{ print $1}' | xargs -n1 groups
 # output gets like 
 #sonaradmin : sonaradmin
 #tester : test
 #dathu : dathu
-sudo amazon-linux-extras install java-openjdk11 -y
-echo "java installed"
+#sudo amazon-linux-extras install java-openjdk11 -y
+#echo "java installed"
 cd /opt/
 #move to opt directory
 pwd
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.6.1.59531.zip
 yum install unzip -y
 unzip sonarqube-9.6.1.59531.zip
-chown -R sonarqube:sonarque /opt/sonarqube-9.6.1.59531
+chown -R gopala:gopala /opt/sonarqube-9.6.1.59531
 chmod -R 777 /otp/sonarqube-9.6.1.59531
 ls -la
 cd /opt/sonarqube-9.6.1.59531/bin/linux-x86-64
